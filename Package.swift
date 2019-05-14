fd29277b9a811b07645003f201dd14c19acb781e
Abdurrahman Avci <abdurrahmanavci@gmail.com>
2019-05-14 23:10:49 +0300

    Switch to swift tools version 4.2.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "CGLFW",
-    pkgConfig: "glfw3",
-    providers: [
-        .apt(["libglfw3-dev"]),
-        .brew(["glfw"])
-    ],
     products: [
         .library(name: "CGLFW", targets: ["CGLFW"])
     ],
     targets: [
-        .target(name: "CGLFW")
+        .systemLibrary(
+            name: "CGLFW",
+            pkgConfig: "glfw3",
+            providers: [
+                .apt(["libglfw3-dev"]),
+                .brew(["glfw"])
+            ]
+        )
     ]
 )
